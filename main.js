de772c43c90665579043397e7d774b5e1fb82ef1
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-27 12:31:51 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+//////////////////////////////////
+//////// PRODUCTS DATA.JS ////////
+//////////////////////////////////
+
+var productsData = {
+  productsList: [
+    {
+      title: "Super Mario World",
+      cost: 11.25,
+      image: "http://ecx.images-amazon.com/images/I/31QRKPQ24FL._AA160_.jpg",
+      details: {
+        players: "1-2",
+        levels: "74 different areas in 7 different castles"
+      }
+    },
+    {
+      title: "Donkey Kong Country",
+      cost: 12.45,
+      image: "http://ecx.images-amazon.com/images/I/31ACJPWHBVL._AA160_.jpg",
+      sale: 5.00,
+      details: {
+        characters: "Donkey Kong and Diddy Kong",
+        enemies: "Kremlings"
+      }
+    },
+    {
+      title: "Street Fighter II",
+      cost: 4.94,
+      image: "http://ecx.images-amazon.com/images/I/51aqQ0RJtxL._AA160_.jpg",
+      details: {
+        description: 'Get dirty with the nasty street fighters in the multi-player Street Fighter II.'
+      }
+    }
+  ]
+};
+
+
+
+$(document).on('ready', function() {
+
+	// Get the HTML content of our template and store it
+	// as a JavaScript variable
+	var templateSource = $('#product-template').html();
+	console.log(templateSource);
+
+	// Have handlebars compile this sources into a
+	// reusable template function
+	var template = Handlebars.compile(templateSource);
+	console.log(template);
+
+	// Use the template to generate new HTML content
+	var outputHTML = template(productsData);
+	console.log(outputHTML);
+
+	// Append outputHTML to the page
+	$('#product-container').append(outputHTML);
+  
+});
